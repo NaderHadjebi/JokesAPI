99a46a0e72e3d5b16fa8663b69b1b126255e0bc4
Nader Hadjebi <60085977+NaderHadjebi@users.noreply.github.com>
2026-04-29 22:27:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using JokeFetcher.Services;
+
+class Program
+{
+    static async Task Main()
+    {
+        var jokeService = new JokeService();
+
+        Console.WriteLine("Fetching a random joke...\n");
+
+        try
+        {
+            var joke = await jokeService.GetRandomJokeAsync();
+            Console.WriteLine(joke);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ Services/JokeService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JokeFetcher.Services
+{
+    public class JokeService
+    {
+        private readonly HttpClient _client;
+
+        public JokeService()
+        {
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add("User-Agent", "CSharp-Joke-App");
+        }
+
+        public async Task<string> GetRandomJokeAsync()
+        {
+            string url = "https://v2.jokeapi.dev/joke/Any";
+
+            var response = await _client.GetStringAsync(url);
+            using JsonDocument doc = JsonDocument.Parse(response);
+
+            var root = doc.RootElement;
+
+            string type = root.GetProperty("type").GetString();
+
+            if (type == "single")
+            {
+                return root.GetProperty("joke").GetString();
+            }
+            else
+            {
+                return root.GetProperty("setup").GetString()
+                    + "\n"
+                    + root.GetProperty("delivery").GetString();
+            }
+        }
+    }
+}
